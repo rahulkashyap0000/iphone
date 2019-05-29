7d7cf3cc4c30131d88b392f855035602e7eff8a2
Rahul Kashyap <rahul.kashyap.17001@iitgoa.ac.in>
2019-05-29 15:40:31 +0530

    Finished the about Page

--- index.js
+++ index.js
@@ -29,3 +29,10 @@ $(document).on("click", '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
 });
+
+//Slick Init
+$(".slider").slick({
+    infinite: true,
+    slideToShow: 1,
+    slideToScroll: 1
+});
